8b9c450d9b74d1ae4a0fb672ed7e1e93fafd0772
kfernando <kriziaf2014@gmail.com>
2016-03-20 22:24:45 -0400

    corrections to string
    added “”

--- js/index.js
+++ js/index.js
@@ -1,39 +1,41 @@
 $(document).ready(function(){
 
 	$("#submit-btn").click(updatePhoto);
-	//listen for click to collect keyword entry
+	//listen for click on id of submit button
+	//collect keyword entry
 
 	function updatePhoto(event) {
 		event.preventDefault();	
 		
-		//store keyword entry
+		//store keyword entry as a variable of city
 		var = city;
 		
 		//get keyword entry
-		city = $("#city-type").val();
+		//convert to lower case
+		city = $("#city-type").val().toLowerCase();
 
 		//validate entry to match proper class in css
 		//use conditionals to add variations of city
 		//change background and choose from class provided from style.css
 
-		if (city === NYC || city === New York City || city === New York) {
-			$("body").attr("class",".nyc");
+		if (city === 'NYC' || city === 'New York City' || city === 'New York' || ) {
+			$("body").attr('class','nyc');
 		};
 
-		else if (city === SF || city === San Francisco || city === Bay Area) {
-			$("body").attr("class",".sf");
+		else if (city === 'SF' || city === 'San Francisco' || city === 'Bay Area') {
+			$("body").attr('class','sf');
 		};
 
-		else if (city === LA || city === Los Angeles || city === LAX) {
-			$("body").attr("class",".la");
+		else if (city === 'LA' || city === 'Los Angeles' || city === 'LAX') {
+			$("body").attr('class','la');
 		};
 
-		else if (city === ATX || city === Austin) {
-			$("body").attr("class",".austin");
+		else if (city === 'ATX' || city === 'Austin') {
+			$("body").attr('class','austin');
 
 		};
-		else if (city === SYD || city === Sydney) {
-			$("body").attr("class",".sydney");
+		else if (city === 'SYD' || city === 'Sydney') {
+			$("body").attr('class','nyc');
 		};
 		
 		else {
