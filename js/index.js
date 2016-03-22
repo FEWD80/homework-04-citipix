27a2d9d8ca92495865b8d8da30c631e473a46f37
gerriecho <gerriecho@gmail.com>
2016-03-22 18:27:27 -0400

    HW4

--- js/index.js
+++ js/index.js
@@ -16,19 +16,19 @@ $(function(){
 		// List the possible city names 
 		if (city === "New York" || city === "New York City" || city === "NYC") {
 			// Change the background image to the corresponding city photo
-			$("body").css("backgroundImage", "url(images/nyc.jpg");
+			$("body").attr("class", "nyc");
 
 		} else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
-			$("body").css("backgroundImage", "url(images/sf.jpg");
+			$("body").attr("class", "sf");
 
 		} else if (city === "Los Angeles" || city === "LA" || city === "LAX" ) {
-			$("body").css("backgroundImage", "url(images/la.jpg");
+			$("body").attr("class", "la");
 		
 		} else if (city === "Austin" || city === "ATX") {
-			$("body").css("backgroundImage", "url(images/austin.jpg");
+			$("body").attr("class", "austin");
 
 		} else if (city === "Sydney" || city === "SYD") {
-			$("body").css("backgroundImage", "url(images/sydney.jpg");
+			$("body").attr("class", "sydney");
 		
 		// If none of the above cities are entered, then alert the user to try another city
 		} else { 
