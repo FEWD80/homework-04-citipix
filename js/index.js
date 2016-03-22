ddcf657a36c9b1e1f4b3631d1aa26a7ebd53e315
kfernando <kriziaf2014@gmail.com>
2016-03-22 18:28:39 -0400

    JS index linked

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(function(){
+
+	var city;
 
 	$("#submit-btn").click(updatePhoto);
 	//listen for click on id of submit button
@@ -10,40 +12,37 @@ $(document).ready(function(){
 		//store keyword entry as a variable of city
 		//get keyword entry
 		var city = $('#city-type').val();
-
-       	//store entry in console
-       	console.log(city);
+		console.log('test');
 
 		//use conditionals for variations of entries per city
 		//validate entry to match proper class in css
 		//change background and choose from class provided in style.css
 
-/*		if (city === 'NYC' || city === 'New York City' || city === 'New York') {
-			$("body").attr('class','nyc');
+		if (city === 'NYC' || city === 'New York City' || city === 'New York') {
+			$('body').attr('class','nyc');
 		}
 
 		else if (city === 'SF' || city === 'San Francisco' || city === 'Bay Area') {
-			$("body").attr('class','sf');
+			$('body').attr('class','sf');
 		}
 
 		else if (city === 'LA' || city === 'Los Angeles' || city === 'LAX') {
-			$("body").attr('class','la');
+			$('body').attr('class','la');
 		}
 
 		else if (city === 'ATX' || city === 'Austin') {
-			$("body").attr('class','austin');
+			$('body').attr('class','austin');
 
 		}
 		else if (city === 'SYD' || city === 'Sydney') {
-			$("body").attr('class','nyc');
+			$('body').attr('class','nyc');
 		}
 
 		else {
-			alert("Sorry," + city + " is unavailable");;
+			alert('Sorry,' + city + ' is unavailable');
 		}
-*/
-	}
 
+	}
 
 });
 
