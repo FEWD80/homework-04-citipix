f76483f0368368e23b3b6a8677938cf31c2e3793
Luis Lithgow <lalithgow@gmail.com>
2016-03-24 17:32:32 -0400

    Got the JS working
    I added the ‘var = city’ on the top before the function, under the
    function declaration.

--- js/index.js
+++ js/index.js
@@ -1,45 +1,63 @@
 $(function() {
 
 
+	
 	$('#submit-btn').click(getCitiPix);
-	var city = $('#city-type').val();
+	var city;
 	// alert('testtop');
+	// event.preventDefault();	
 
 		// create function called getCitiPix
-	function getCitiPix() {
+	function getCitiPix(event) {
+		// event.preventDefault();	
 		var city = $('#city-type').val();
-		$.ready();
-		event.preventDefault;
-				
-		conole.log('testing1');
+		event.preventDefault();	
+	
 
 		// Create variable(s) for site
-		
 		// Get city-type, convert and store in variable
-
 		// Submit button should submit the data and pull up 
-		
 		// If user inputs(...), then background img should chnage
 		// if (true) {}
-		
-		if (city === 'nyc' || 'new york' || 'ny' || 'big apple') {
-			$('body').css('backgroundImage', 'url(../images/nyc.jpg)') ;
+// New York City	
+		if (city === 'nyc' || city ==='NYC' || city === 'New York City'|| 
+			city === 'new york city'|| city === 'new york' || city ==='New York'|| 
+			city === 'ny' || city ==='NY'|| city === 'big apple') {
+			$('body').css('backgroundImage', 'url(images/nyc.jpg)');
+			// console.log('am I here?');
 
-		} else if (city === 'sf' || 'san francisco'|| 'bay area') {
+
+// San Francisco
+		} else if (city === 'sf' || city ==='SF' || city ==='San Francisco' || 
+			city === 'san francisco'|| city === 'bay area') {
 			$('body').css('backgroundImage', 'url(images/sf.jpg)');
 
-		} else if (city === 'la' || 'lax' || 'los angeles' ) {
-			$('body').css('backgroundImage', 'url(/images/la.jpg)');
-		} else if (city === 'atx' || 'austin') {
-			$('body').css('backgroundImage', 'url(images/austin.jpg');
-			// else if (city === )
-		} else if (city === 'syd' || 'sydney') {
-			$('body').css('backgroundImage', 'url(/images/sydney.jpg)');
-		} else {
-			
-		};
-	}
 
-	
+// Los Angeles
+		} else if (city === 'la' || city === 'LA' || city === 'lax' || city === 'LAX' || 
+			city === 'los angeles' || city ==='Los Angeles' ) {
+			$('body').css('backgroundImage', 'url(images/la.jpg)');
+
+
+// Austin
+		} else if (city === 'atx' || city === 'ATX' || city === 'austin' || city === 'Austin') {
+			$('body').css('backgroundImage', 'url(images/austin.jpg');
+		
+
+// Sydney		
+		} else if (city === 'syd' || city ==='SYD' || city ==='Sydney' || city === 'sydney') {
+			$('body').css('backgroundImage', 'url(images/sydney.jpg)');
+		
+
+		} else  { $('body').css('backgroundImage', 'url(images/citipix_skyline.jpg)');
+			// pop-up alert if the city user searched for is not available
+			alert('Sorry, your city is not available at this moment. Come back soon!');
+		}
+	};		
 	
 });
+
+
+
+
+
