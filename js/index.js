416a7bd00d846c77b4987594b187b650bc0cd381
dlevkovits <dlevkovits@gmail.com>
2016-04-05 18:21:30 -0400

    Indentation corrected

--- js/index.js
+++ js/index.js
@@ -6,29 +6,27 @@ $(function(){
 	$("#submit-btn").click(changeCity);
 	
 	//When clicking prevet default
-	function changeCity(event){
-	event.preventDefault();
-	textInput = $("#city-type").val();//.toLowerCase();
-	//If retrieved text =LA
-	if (textInput === "LA" || textInput === "los angeles" || textInput === "Los Angeles"){
-    $('body').attr('class' , 'la');
-    }
+	function changeCity(event) {
+    	event.preventDefault();
+    	textInput = $("#city-type").val();//.toLowerCase();
+    	//If retrieved text =LA
+    	if (textInput === "LA" || textInput === "los angeles" || textInput === "Los Angeles"){
+            $('body').attr('class' , 'la');
+        } else if (textInput === "NY" || textInput === "New York" || textInput === "new york"){
+            $('body').attr('class' , 'nyc');
+        }
 
-    else if (textInput === "NY" || textInput === "New York" || textInput === "new york"){
-    $('body').attr('class' , 'nyc');
-    }
+        if (textInput === "SF" || textInput === "San Francisco" || textInput === "san francisco"){
+            $('body').attr('class' , 'sf');
+        }
 
-    if (textInput === "SF" || textInput === "San Francisco" || textInput === "san francisco"){
-    $('body').attr('class' , 'sf');
-    }
+        if (textInput === "Sidney" || textInput === "sidney"){
+            $('body').attr('class' , 'sidney');
+        }
 
-    if (textInput === "Sidney" || textInput === "sidney"){
-    $('body').attr('class' , 'sidney');
-    }
-
-    if (textInput === "Austin" || textInput === "austin"){
-    $('body').attr('class' , 'austin');
-    }
+        if (textInput === "Austin" || textInput === "austin"){
+            $('body').attr('class' , 'austin');
+        }
 	}
 
 });
